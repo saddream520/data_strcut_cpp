3ed431cefeb9ae586a5bae92fde06803aa3f018e
saddream <abc2807677441@163.com>
2026-07-31 23:27:06 +0800

    知识点注释

--- hash/unordered_map.cpp
+++ hash/unordered_map.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 int main()
 {
+    // 利用键值对映射的方法，我们可以做到用空间换时间，极大提高效率
+    // 使用范围不亚于查重，寻找目标值等
     unordered_map<string,string>text;
     text["zoo"] = "zoo_visit";
     text["car"] = "Toyota";
